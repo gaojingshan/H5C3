3a6f1a49ba38cabf3acabf06be85372e0347ede1
高境杉 <1359287622@qq.com>
2020-04-14 10:12:14 +0800

    360触摸显示效果

--- js/media13.js
+++ js/media13.js
@@ -2,8 +2,9 @@
 
 $('.tab span').click(function () {
   var offset_left = $(this).offset().left;
-//   console.log(getComputedStyle($('.tab span')).width);
-//   console.log($('.tab span').eq(0).width);
+  //   console.log(getComputedStyle($('.tab span')).width);
+  var index = $(this).index();
+  console.log($('.tab span').eq(index).css('width'));
   $('.tab i').css('left', offset_left);
 });
 // console.log();
