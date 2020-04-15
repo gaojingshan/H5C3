625c3cf680d306ec810e708da72d288844431138
高境杉 <1359287622@qq.com>
2020-04-15 18:34:55 +0800

    缩略图相册特效

--- js/Thumbnail_album.js
+++ js/Thumbnail_album.js
@@ -1,4 +1,16 @@
-$('.container .mainframe li:hover .text p'){
-    p.text()
-    
-}
+// 获取img上面的alt，赋值给它下面的p标签
+$('.container .mainframe li').mouseenter(function () {
+  var p_text = $(this).children()[0].getAttribute('alt');
+  $(this).children().eq(1).children().eq(0).text(p_text);
+});
+
+// 点击缩略图在左边放大显示
+$('#container .thumbnails .thumbnailimage').click(function () {
+  var index = $(this).index();
+  // console.log(index);
+  $('#container .mainframe li')
+    .eq(index + 1)
+    .slideDown()
+    .siblings()
+    .slideUp();
+});
