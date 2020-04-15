0e4c105a0d057f7d23d3b86d80f2c48fcf0af198
高境杉 <1359287622@qq.com>
2020-04-15 16:43:52 +0800

    缩略图相册特效

---
+++ js/Thumbnail_album.js
@@ -0,0 +1,4 @@
+$('.container .mainframe li:hover .text p'){
+    p.text()
+    
+}
--- js/accordion.js
+++ js/accordion.js
@@ -1,19 +1,21 @@
 // 手风琴
 var lock = true;
-var index;
-var ul_index;
-$('#accordion ul li').mouseenter(function () {
-  if (!lock) {
+var index = 0;
+var ul_index = 0;
+// 看鼠标在不在元素里面
+var newlock = true;
+function enter(index1, ul_index1) {
+  /* if (!lock) {
     return;
   }
   lock = false;
   setTimeout(function () {
     lock = true;
-  }, 600);
+  }, 600); */
   // 每排的li索引
-  index = $(this).index();
+  index = index1;
   //  ul索引
-  ul_index = $(this).parent('ul').index();
+  ul_index = ul_index1;
 
   // 与点击元素不同行不同列的
   $('#accordion ul li').css({
@@ -25,15 +27,20 @@ $('#accordion ul li').mouseenter(function () {
     width: '375',
     height: '70',
   });
-  /* $('#accordion ul li').eq(index).css({
-    width: '375',
-    height: '70',
-  }); */
   // 同行同ul索引
   $(`#accordion ul:nth-child(${ul_index + 1}) li`).css({
     width: '146.25',
     height: '470',
   });
+}
+$('#accordion ul li').mouseenter(function () {
+  newlock = false;
+  // 每排的li索引
+  index = $(this).index();
+  //  ul索引
+  ul_index = $(this).parent('ul').index();
+
+  enter(index, ul_index);
   // 点击的元素
   $(this).css({
     width: '375',
@@ -42,18 +49,35 @@ $('#accordion ul li').mouseenter(function () {
 });
 
 $('#accordion ul li').mouseleave(function () {
-  if (!lock) {
-    $('#accordion ul li').css({
-        width: '192',
-        height: '170',
-      });
-  }
+  newlock = true;
+  // if (!lock) {
+  $('#accordion ul li').css({
+    width: '192',
+    height: '170',
+  });
+  // }
 });
 
 setInterval(function () {
-  if (lock) {
-    /* index++;
-    console.log(index); */
-    // $('#accordion ul li').trigger('mouseenter');
+  if (newlock) {
+    index++;
+    if (index > 4) {
+      index = 0;
+      ul_index++;
+      if (ul_index > 3) {
+        index = 0;
+        ul_index = 0;
+      }
+    }
+    enter(index, ul_index);
+    // 点击的元素
+    // 同列同li索引
+    $(`#accordion ul li:nth-child(${index + 1})`).css({
+      width: '375',
+    });
+    // 同行同ul索引
+    $(`#accordion ul:nth-child(${ul_index + 1}) li`).css({
+      height: '470',
+    });
   }
-}, 1000);
+}, 3000);
